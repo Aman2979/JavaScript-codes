afcc01fbf7bad8a1d70ef2834fa1ff9ecb2c545a
Aman Kumar <ak93896897@gmail.com>
2025-05-04 09:53:10 +0000

    Varibles & DataTpes

---
+++ Basics/01_varibles.js
@@ -0,0 +1,16 @@
+const accountId = 14453;
+let accountEmail = 'ak93896889@gmail.com';
+var accoutPassword = "Aman@2979"; // -> Issue of scope
+accountCity = 'Moradabad';
+let accountState;
+
+// accountId = 2  //not allowed
+
+accountEmail = 'Changed'
+accountEmail = 'Changed1'
+accountEmail = 'Changed2'
+accoutPassword = '1441'
+accountCity = "Kanth"
+// console.log(accountEmail);
+
+console.table([accountEmail, accountId, accoutPassword, accountCity, accountState])
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js code newer version.
+
+// alert("Aman") // we are using nodejs, not brower
+
+let age = 18;
+let isloggedIn = false;
+let state = "null";
+let nullValue = null; 
+
+// number => 2 to power 53
+// bigint
+// strint => ""
+// boolean => true/false
+// null => standlone Value
+// undefined
+// symbol => unique
+
+
+//objets
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
